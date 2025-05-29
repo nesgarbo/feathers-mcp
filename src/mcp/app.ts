8805878015db7dae4349ac2370cebb93b0f894eb
nesgarbo <nesgarbo@gmail.com>
2025-05-29 10:29:42 +0200

    Improved koa-express

--- src/mcp/app.ts
+++ src/mcp/app.ts
@@ -3,6 +3,6 @@ import type { Application as KoaApplication } from '@feathersjs/koa'
 import type { Application as ExpressApplication } from '@feathersjs/express'
 export type McpApplication = KoaApplication | ExpressApplication;
 
-export function isKoaApplication(app: McpApplication): app is KoaApplication {
-  return typeof (app as any).callback === 'function' && Array.isArray((app as any).middleware);
+export function isKoaApp(app: any): app is KoaApplication {
+  return 'context' in app;
 }
